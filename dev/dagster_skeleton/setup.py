fd9f06296090c9aeda74b0dbe3c4a59aafa5480f
ttt <pedroallenrevez@gmail.com>
2023-06-02 15:07:55 +0100

    chore: remove no longer needed dev folder

--- dev/dagster_skeleton/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="dagster_skeleton",
-    packages=find_packages(exclude=["dagster_skeleton_tests"]),
-    install_requires=["dagster", "dagster-cloud"],
-    extras_require={"dev": ["dagit", "pytest"]},
-)
